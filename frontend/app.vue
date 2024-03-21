7f7f1153d2945d8177757321018eca860b12e1a9
laynaria <pro.laynaria@outlook.fr>
2024-03-21 15:37:10 +0100

    Setup of Nuxt + Tailwind + Apollo for Frontend

---
+++ frontend/app.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const query = gql`
+  query getShips($limit: Int!) {
+    ships(limit: $limit) {
+      id
+      name
+    }
+  }
+`;
+
+const variables = { limit: 5 };
+
+const { data }: any = await useAsyncQuery(query, variables);
+</script>
+
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+  <p class="bg-red-400">TEST</p>
+  <p>There are {{ data?.ships?.length || 0 }} ships.</p>
+</template>
