4fee09e06d26d68dff8cc5c16301de5f604b5b81
laynaria <pro.laynaria@outlook.fr>
2024-03-22 09:25:34 +0100

    initialized backend and dockerized backend

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Frontend : Nuxt + Apollo + Tailwind
 
 ## Backend : GraphQL + Apollo + TypeGraphQL + TypeORM
+
+Next Task:
+
+- Docker compose
+- Check if backend work
+
+- Dockerize frontend
---
+++ backend/src/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  username: string;
+
+  @Field()
+  @Column()
+  email: string;
+
+  @Field()
+  @Column()
+  password: string;
+}
