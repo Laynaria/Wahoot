09710281e45ebe3ab8418b54ab0bfe0ea9f3c0e1
laynaria <pro.laynaria@outlook.fr>
2024-03-24 10:32:49 +0100

    added register in backend

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { User } from "../entities/user.entity";
 import * as UserService from "../services/user.service";
 
@@ -7,5 +7,19 @@ export class UserResolver {
   @Query(() => [User])
   async getUsers(): Promise<User[]> {
     return UserService.findAll();
+  }
+
+  @Query(() => User)
+  async getUserById(@Arg("id") id: number): Promise<User> {
+    return UserService.findById(id);
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("email") email: string,
+    @Arg("username") username: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    return UserService.createUser(email, username, password);
   }
 }
