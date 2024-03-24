e5405f957aa65fa1376d3333266222f2058db137
laynaria <pro.laynaria@outlook.fr>
2024-03-24 10:15:49 +0100

    backend link now in a env file for nuxt config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
     clients: {
       default: {
         // httpEndpoint: "http://backend:5010/graphql",
-        httpEndpoint: "http://localhost:5010/graphql",
+        httpEndpoint: process.env.BACKEND_URL as string,
       },
     },
   },
