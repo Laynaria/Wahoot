fee66e4475d6aac68c936f2c30395a66a7119d59
laynaria <pro.laynaria@outlook.fr>
2024-04-14 09:25:16 +0200

    externalized server setup in a createServer function in server.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,13 @@
 import "reflect-metadata";
-import { dataSource } from "./config/db";
-import { ApolloServer } from "apollo-server";
+import createServer from "./config/server";
 import * as dotenv from "dotenv";
-import { buildSchema } from "type-graphql";
-import { UserResolver } from "./resolvers/user.resolver";
+
+dotenv.config();
+
+const port: number = parseInt(process.env.PORT as string);
 
 const start = async () => {
-  dotenv.config();
-  const port: number = parseInt(process.env.PORT as string);
-  await dataSource.initialize();
-
-  const schema = await buildSchema({
-    resolvers: [UserResolver],
-    validate: { forbidUnknownValues: false },
-  });
-
-  const server = new ApolloServer({ schema });
+  const server = await createServer();
 
   try {
     const { url } = await server.listen({ port });
