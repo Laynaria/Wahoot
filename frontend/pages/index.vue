3311132f899234b2f9b5b49f1c0b5e972cbf5e66
laynaria <pro.laynaria@outlook.fr>
2024-04-14 10:27:23 +0200

    conditionnal rendering on index page based if user is logged or not

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -12,15 +12,18 @@ const GET_ALL_USERS = gql`
   }
 `;
 
-const { data } = await useLazyAsyncQuery<UserResults>(GET_ALL_USERS);
+const { data, error } = await useLazyAsyncQuery<UserResults>(GET_ALL_USERS);
 </script>
 
 <template>
   <p>HOME</p>
 
   <p class="bg-red-400">TEST</p>
-  <p>There are {{ data?.getUsers?.length || 0 }} users.</p>
-  <p>{{ data?.getUsers[0].username }}</p>
-  <p>{{ data?.getUsers[0].email }}</p>
-  <p>{{ data?.getUsers[0].id }}</p>
+
+  <p v-if="error">You don't have authorization to see</p>
+
+  <p v-if="!error">There are {{ data?.getUsers?.length || 0 }} users.</p>
+  <p v-if="!error">{{ data?.getUsers[0].username }}</p>
+  <p v-if="!error">{{ data?.getUsers[0].email }}</p>
+  <p v-if="!error">{{ data?.getUsers[0].id }}</p>
 </template>
