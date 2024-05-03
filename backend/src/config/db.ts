7e6b18a3f920a61942a0b54819fdb3b39c8ae6f9
laynaria <pro.laynaria@outlook.fr>
2024-05-03 11:26:22 +0200

    reworked backend and docker compose environement variables to hide everything sensitive

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,11 +4,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "db",
+  host: process.env.HOST,
   port: parseInt(process.env.DB_PORT as string),
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  entities: ["src/entities/*.ts"],
+  entities: ["src/entities/*.{ts,js}"],
   synchronize: true,
 });
