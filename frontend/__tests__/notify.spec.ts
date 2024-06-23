4863f3c79c5ce7d58968a03de961b03c79c9f7c0
laynaria <pro.laynaria@outlook.fr>
2024-06-23 12:05:21 +0200

    improved notification test from previous branch, by using a wrapper

---
+++ frontend/__tests__/notify.spec.ts
@@ -0,0 +1,60 @@
+import { render, fireEvent, screen } from "@testing-library/vue";
+import NotifyWrapper from "./wrappers/NotifyWrapper.vue";
+
+import * as NotifyService from "~/services/notify";
+
+const { notifySuccess, notifyError, notifyInfo, notifyWarn } = NotifyService;
+
+describe("Success Notifications", () => {
+  it("Success Notification function to exist", () => {
+    expect(notifySuccess).toBeTruthy();
+    expect(typeof notifySuccess).toBe("function");
+  });
+
+  it("Success Toast rendered properly when funciton is called", async () => {
+    render(NotifyWrapper);
+
+    const button = await screen.findByText("Success Button");
+
+    await fireEvent.click(button);
+
+    const notification = await screen.findByText("test Successfull");
+
+    expect(notification.innerHTML).toBe("test Successfull");
+  });
+});
+
+describe("Error Notifications", () => {
+  it("Error Notification function to exist", () => {
+    expect(notifyError).toBeTruthy();
+    expect(typeof notifyError).toBe("function");
+  });
+
+  it("Error Toast rendered properly when funciton is called", async () => {
+    render(NotifyWrapper);
+
+    const button = await screen.findByText("Error Button");
+
+    await fireEvent.click(button);
+
+    const notification = await screen.findByText("test Error");
+
+    expect(notification.innerHTML).toBe("test Error");
+  });
+});
+
+//     it("Error Notification function to exist", () => {
+//       expect(notifyError).toBeTruthy();
+//       expect(typeof notifyError).toBe("function");
+//     });
+
+//     it("Info Notification function to exist", () => {
+//       expect(notifyInfo).toBeTruthy();
+//       expect(typeof notifyInfo).toBe("function");
+//     });
+
+//     it("Warn Notification function to exist", () => {
+//       expect(notifyWarn).toBeTruthy();
+//       expect(typeof notifyWarn).toBe("function");
+//     });
+//   });
---
+++ frontend/__tests__/wrappers/NotifyWrapper.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import * as NotifyService from "~/services/notify";
+import { ref } from "vue";
+
+const { notifySuccess, notifyError, notifyInfo, notifyWarn } = NotifyService;
+
+// const value = ref("");
+
+// const success = async () => {
+//   await notifySuccess(`test`);
+//   value.value = "success";
+// };
+</script>
+
+<template>
+  <button @click="notifySuccess(`test`)">Success Button</button>
+</template>
