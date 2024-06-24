2430986ea099099365ba4e4cbb81af934845288e
laynaria <pro.laynaria@outlook.fr>
2024-06-24 16:31:36 +0200

    description added to seo

--- frontend/services/meta.ts
+++ frontend/services/meta.ts
@@ -3,5 +3,11 @@ export const meta = (title: string, lang: string) => {
     title: `Wahoot - ${title}`,
     htmlAttrs: { lang },
     link: [{ rel: "icon", href: "~/favicon.ico" }],
+    meta: [
+      {
+        name: "description",
+        content: `${title} page of Wahoot project. An icebreaker web application where people can learn while having fun!`,
+      },
+    ],
   });
 };
