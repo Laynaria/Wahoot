689b3bb6d7a246d7194e9ba504c05b75e1fbb84a
laynaria <pro.laynaria@outlook.fr>
2024-06-24 16:10:59 +0200

    added a favicon management in meta service

--- frontend/services/meta.ts
+++ frontend/services/meta.ts
@@ -2,5 +2,6 @@ export const meta = (title: string, lang: string) => {
   useHead({
     title: `Wahoot - ${title}`,
     htmlAttrs: { lang },
+    link: [{ rel: "icon", href: "~/favicon.ico" }],
   });
 };
